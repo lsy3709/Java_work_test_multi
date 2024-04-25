b35477e8d4172caaa584eef44ce53aa07dbd3358
LeeSangYong <lsy3709@gmail.com>
2024-04-25 17:07:25 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Java_work_test_multi" 
---
+++ java_work_test2/src/java_work_test2/test/Hello.java
@@ -0,0 +1,10 @@
+package java_work_test2.test;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("test");
+
+	}
+
+}
